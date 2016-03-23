aad7b60e935fb37a99dec296eb0c22b1e6d4c570
Austin Tse <austintse@Austins-MacBook-Pro.local>
2016-03-22 20:15:25 -0700

    added config example, deleted bin executable, and moved server start to app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var express = require('express'),
-    path = require('path'),
+    path    = require('path'),
     favicon = require('serve-favicon'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    config = require('./config');
-    i18n = require("i18n");
+    logger  = require('morgan'),
+    cookieParser    = require('cookie-parser'),
+    bodyParser      = require('body-parser'),
+    config          = require('./config'),
+    i18n            = require('i18n'),
+    debug           = require('debug')('slack-invite-automation:server'),
+    http            = require('http'),
+    
+    routes          = require('./routes/index'),
 
-var routes = require('./routes/index');
-
-var app = express();
+    app = express();
 
 i18n.configure({
     defaultLocale: "en",
@@ -66,6 +68,60 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = parseInt(process.env.PORT, 10) || 3000;
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error('Port ' + port + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error('Port ' + port + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  console.log('Server listening on port: ', port);
+  debug('Listening on port ' + server.address().port);
+}
 
 
 module.exports = app;
